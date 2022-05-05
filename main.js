6ccf5ddf8088a9f192509f17af2b6a384399446a
SpidySamurai <javierchiortiz@gmail.com>
2022-05-04 23:21:39 -0500

    Using async await sintaxis

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-console.log("xd");
-
 const CAT_API = "https://api.thecatapi.com/v1/images/search";
 
-fetch(CAT_API)
-  .then((res) => res.json())
-  .then((data) => {
-    const img = document.getElementById("img-container");
-    img.src = data[0].url;
-  });
+// fetch(CAT_API)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     const img = document.getElementById("img-container");
+//     img.src = data[0].url;
+//   });
 
-const onClickFetch = () => {
-  fetch(CAT_API)
-    .then((res) => res.json())
-    .then((data) => {
-      const img = document.getElementById("img-container");
-      img.src = data[0].url;
-    });
+const onClickFetch = async () => {
+  const response = await fetch(CAT_API);
+  const data = await response.json();
+
+  const img = document.getElementById("img-container");
+  img.src = data[0].url;
 };
