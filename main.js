6cd1708b1d4bfa109538132b00cbd72b397ddcbf
SpidySamurai <javierchiortiz@gmail.com>
2022-05-09 15:34:51 -0500

    Query paramater and pagination

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-const CAT_API = "https://api.thecatapi.com/v1/images/search";
+const CAT_API = "https://api.thecatapi.com/v1/images/search?limit=3";
+//query parameter, you can request 1, and more data objects
 
-// fetch(CAT_API)
-//   .then((res) => res.json())
-//   .then((data) => {
-//     const img = document.getElementById("img-container");
-//     img.src = data[0].url;
-//   });
+fetch(CAT_API)
+  .then((res) => res.json())
+  .then((data) => {
+    const img1 = document.getElementById("img-container-1");
+    img1.src = data[0].url;
+
+    const img2 = document.getElementById("img-container-2");
+    img2.src = data[1].url;
+
+    const img3 = document.getElementById("img-container-3");
+    img3.src = data[2].url;
+  });
 
 const onClickFetch = async () => {
   const response = await fetch(CAT_API);
   const data = await response.json();
 
-  const img = document.getElementById("img-container");
-  img.src = data[0].url;
+  const img1 = document.getElementById("img-container-1");
+  img1.src = data[0].url;
+
+  const img2 = document.getElementById("img-container-2");
+  img2.src = data[1].url;
+
+  const img3 = document.getElementById("img-container-3");
+  img3.src = data[2].url;
 };
