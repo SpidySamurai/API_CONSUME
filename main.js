f7dd6895aed57a9ac5199b9ee9600ece8b2eb642
SpidySamurai <javierchiortiz@gmail.com>
2022-05-10 22:23:09 -0500

    Style adjustment

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
-const CAT_API =
-  "https://api.thecatapi.com/v1/images/search?limit=3&api_key=8b8d2f7c-74e4-47cd-83c2-b03ee2c82027";
+const CAT_API = "https://api.thecatapi.com/v1/";
+const API_KEY = "8b8d2f7c-74e4-47cd-83c2-b03ee2c82027";
 //query parameter, you can request 1, and more data objects
 
-fetch(CAT_API)
-  .then((res) => res.json())
-  .then((data) => {
-    const img1 = document.getElementById("img-container-1");
-    img1.src = data[0].url;
-
-    const img2 = document.getElementById("img-container-2");
-    img2.src = data[1].url;
-
-    const img3 = document.getElementById("img-container-3");
-    img3.src = data[2].url;
-  });
-
-const onClickFetch = async () => {
-  const response = await fetch(CAT_API);
+const loadRandomCats = async () => {
+  const response = await fetch(
+    `${CAT_API}images/search?limit=3&api_key${API_KEY}`
+  );
   const data = await response.json();
 
   const img1 = document.getElementById("img-container-1");
@@ -28,3 +17,5 @@ const onClickFetch = async () => {
   const img3 = document.getElementById("img-container-3");
   img3.src = data[2].url;
 };
+
+loadRandomCats();
